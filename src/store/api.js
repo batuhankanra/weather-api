f786412cc5d25df07ae38802d57ffc4e97e813fb
batuhan <batuhan_kanra_60@hotmail.com>
2025-02-01 22:40:14 +0300

    updated

--- src/store/api.js
+++ src/store/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const token=import.meta.env.VITE_API_TOKEN
 export const getAPI=createAsyncThunk('getAPI',async (request)=>{
-    const response =await axios.get(`http://api.weatherapi.com/v1/current.json/forecast.json?days=2025-02-01&key=${token}&q=${request.city}&lang=${request.lang}`)  
+    const response =await axios.get(`https://api.weatherapi.com/v1/current.json/forecast.json?days=2025-02-01&key=${token}&q=${request.city}&lang=${request.lang}`)  
       console.log(response.data)
       return response.data   
 })
